df1f8b15847f33f1fb7678e53359460bc3c96e88
tukiko-lilii-borea <k.family.sats@gmail.com>
2026-05-08 22:58:35 +0900

    fix: add config.js for GitHub Pages

---
+++ config.js
@@ -0,0 +1,10 @@
+const PLACES_API_KEY = 'AIzaSyBJBgC5uSO2J0MV7Hlwdt7zY8SxLUvZbP8';
+const WEATHER_API_KEY = '09b8e0e5cdae5008f9471a84841b6b51';
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAhNAZjlyohQaYzB8HjX5qTpW5W29JrOfw",
+  authDomain: "nikowa2525.firebaseapp.com",
+  projectId: "nikowa2525",
+  storageBucket: "nikowa2525.firebasestorage.app",
+  messagingSenderId: "579205168629",
+  appId: "1:579205168629:web:c3fd1ede3a91886d7274bc"
+};
