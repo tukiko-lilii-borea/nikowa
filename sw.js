5cf3f68a56f069a16dfc77df6eaeb303248d69fb
kfamilysats-collab <k.family.sats@gmail.com>
2026-05-13 17:17:39 +0900

    sw.js 自動更新対応

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
-const CACHE = 'nikowa-v1';
-const FILES = ['/index.html'];
+const CACHE = 'nikowa-v' + Date.now();
+const FILES = ['/nikowa/index.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() =>
+    caches.match(e.request)
+  ));
 });
